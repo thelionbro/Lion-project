a5764e4b3122754d1a60b70a18a44ca11b50c2c8
HeyyNicyy <89997807+HeyyNicyy@users.noreply.github.com>
2022-04-14 11:04:15 +0530

    Update batch.py

--- main/plugins/batch.py
+++ main/plugins/batch.py
@@ -38,7 +38,7 @@ async def _batch(event):
         await event.reply(r)
         return       
     if f'{event.sender_id}' in batch:
-        return await event.reply("You've already started one batch, wait for it to complete you dumbfuck owner!")
+        return await event.reply("You've already started one batch, wait for it to complete you dumb!")
     async with Drone.conversation(event.chat_id) as conv: 
         if s != True:
             await conv.send_message("Send me the message link you want to start saving from, as a reply to this message.", buttons=Button.force_reply())
